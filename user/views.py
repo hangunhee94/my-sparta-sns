2d13948131cdb26e13c9243b4743b267e4172298
hangunhee94 <webzen9422@gmail.com>
2022-08-18 16:10:48 +0900

    DELETE- 주석 제거 // 코드정리

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from . models import UserModel
 from django.http import HttpResponse
-from django.contrib.auth import get_user_model # 사용자가 데이터베이스 안에 있는지 검사하는 함수
+from django.contrib.auth import get_user_model
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -19,7 +19,6 @@ def sign_up_view(request):
         bio = request.POST.get('bio', )
 
         if password != password2:
-            #password가 같지 않을때 알람
             return render(request, 'user/signup.html',{'error':'패스워드를 확인 해주세요!'})
         else:
             if username == '' or password == '':
@@ -55,15 +54,11 @@ def logout(request):
     auth.logout(request)
     return redirect('/')
 
-# user/views.py
-
 @login_required
 def user_view(request):
     if request.method == 'GET':
-        # 사용자를 불러오기, exclude와 request.user.username 를 사용해서 '로그인 한 사용자'를 제외하기
         user_list = UserModel.objects.all().exclude(username=request.user.username)
         return render(request, 'user/user_list.html', {'user_list': user_list})
-
 
 @login_required
 def user_follow(request, id):
